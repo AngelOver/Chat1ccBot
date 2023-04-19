23016ced210ce97b40bd6cd2435e32050933bbe6
Jamal Johnson <you@example.com>
2023-04-18 20:22:03 -0400

    图片生成

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -68,6 +68,10 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
   const chatContainerRef = useRef<HTMLDivElement>(null);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
+  const [generateImage, setGenerateImage] = useState(
+    selectedConversation?.generateImage || false,
+  );
+
   const handleSend = useCallback(
     async (message: Message, deleteCount = 0, plugin: Plugin | null = null) => {
       if (selectedConversation) {
@@ -80,11 +84,13 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
           updatedConversation = {
             ...selectedConversation,
             messages: [...updatedMessages, message],
+            generateImage,
           };
         } else {
           updatedConversation = {
             ...selectedConversation,
             messages: [...selectedConversation.messages, message],
+            generateImage,
           };
         }
         homeDispatch({
@@ -99,6 +105,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
           key: apiKey,
           prompt: updatedConversation.prompt,
           temperature: updatedConversation.temperature,
+          generateImage,
         };
         const endpoint = getEndpoint(plugin);
         let body;
@@ -251,6 +258,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       pluginKeys,
       selectedConversation,
       stopConversationRef,
+      generateImage,
     ],
   );
 
@@ -314,6 +322,10 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
   //     homeDispatch({ field: 'currentMessage', value: undefined });
   //   }
   // }, [currentMessage]);
+
+  useEffect(() => {
+    setGenerateImage(selectedConversation?.generateImage || false);
+  }, [selectedConversation]);
 
   useEffect(() => {
     throttledScrollDown();
@@ -433,6 +445,16 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                           })
                         }
                       />
+
+                      <div className="flex items-center">
+                        <input
+                          type="checkbox"
+                          className="form-checkbox"
+                          checked={generateImage}
+                          onChange={(e) => setGenerateImage(e.target.checked)}
+                        />
+                        <span className="ml-2">Generate Image</span>
+                      </div>
                     </div>
                   )}
                 </div>
