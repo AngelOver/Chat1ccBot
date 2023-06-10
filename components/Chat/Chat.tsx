55037233e80329c2e3e6afb0298303549dd6b3a8
zhangliang <2424123787@qq.com>
2023-06-10 23:48:20 +0800

    修改第一次提示语

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -361,13 +361,13 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
 
   return (
     <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#343541]">
-      {!(apiKey || serverSideApiKeyIsSet) ? (
+      {!(apiKey || serverSideApiKeyIsSet)&&false ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
             1Chat
           </div>
           <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
+            {/*<div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>*/}
             <div className="mb-2 font-bold">
               {/*xxxxxxxxxxxx*/}
             </div>
@@ -477,13 +477,13 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                     <IconClearAll size={18} />
                   </button>
                 </div>
-                {showSettings && (
-                  <div className="flex flex-col space-y-10 md:mx-auto md:max-w-xl md:gap-6 md:py-3 md:pt-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
-                    <div className="flex h-full flex-col space-y-4 border-b border-neutral-200 p-4 dark:border-neutral-600 md:rounded-lg md:border">
-                      <ModelSelect />
-                    </div>
-                  </div>
-                )}
+                {/*{showSettings && (*/}
+                {/*  <div className="flex flex-col space-y-10 md:mx-auto md:max-w-xl md:gap-6 md:py-3 md:pt-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">*/}
+                {/*    <div className="flex h-full flex-col space-y-4 border-b border-neutral-200 p-4 dark:border-neutral-600 md:rounded-lg md:border">*/}
+                {/*      <ModelSelect />*/}
+                {/*    </div>*/}
+                {/*  </div>*/}
+                {/*)}*/}
 
                 {selectedConversation?.messages.map((message, index) => (
                   <MemoizedChatMessage
