becaab594b40d9e0478a12bdd2ebfaf7f1107b81
zhangliang <2424123787@qq.com>
2023-06-11 18:21:08 +0800

    调整样式

--- components/Chat/ChatMessage.tsx
+++ components/Chat/ChatMessage.tsx
@@ -126,16 +126,15 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
 
   return (
     <div
-      className={`group md:px-4 ${
-        message.role === 'assistant'
+      className={`group md:px-4 
+        ${message.role === 'assistant'
           ? 'border-b border-black/10 bg-gray-50 text-gray-800 dark:border-gray-900/50 dark:bg-[#444654] dark:text-gray-100'
           : 'border-b border-black/10 bg-white text-gray-800 dark:border-gray-900/50 dark:bg-[#343541] dark:text-gray-100'
       }`}
       style={{ overflowWrap: 'anywhere' }}
     >
       <div className={`
-        ${
-          message.role === 'assistant'
+        ${message.role === 'assistant'
               ? '  m-auto flex p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl'
               : ' flex-row-reverse m-auto flex p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl '
       }`}
@@ -191,8 +190,8 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
                   </div>
                 </div>
               ) : (
-                <div className={` 
-                 ${ message.role === 'assistant'
+                <div className={`
+                      ${message.role === 'assistant'
                         ? '  prose whitespace-pre-wrap dark:prose-invert flex-1'
                         : ' text-right prose whitespace-pre-wrap dark:prose-invert flex-1'
                 }`}>
